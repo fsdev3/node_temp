ea05d8115c5220d6c0f03b88b699c08e1de5f0df
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-10-14 12:03:01 +0200

    upd

--- mentor-lesson1/calculator/index.js
+++ mentor-lesson1/calculator/index.js
@@ -1,4 +1,31 @@
 // console.log("hello from Oleg");
-const { calculate, operator, numbersArr } = require("./lib");
-const result = calculate(operator, numbersArr);
-console.log(result);
+// const { calculate, operator, numbersArr } = require("./lib");
+// const result = calculate(operator, numbersArr);
+// console.log(result);
+const objectA = {
+  a: 10,
+  b: 20,
+  city: "London",
+  country: "United States",
+};
+const objectB = objectA;
+delete objectA.a;
+const userName = "name";
+objectA[userName] = "Andy";
+
+// console.log(objectB);
+
+const objOne = {
+  city: "London",
+  country: "Great Britain",
+  greeteng() {
+    console.log("Hello!!");
+  },
+};
+
+const objString = JSON.stringify(objOne);
+const objParse = JSON.parse(objString);
+
+// objOne.greeteng();
+console.log(objString);
+console.log(objParse);
